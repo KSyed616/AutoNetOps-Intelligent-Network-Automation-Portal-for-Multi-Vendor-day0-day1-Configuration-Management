597e025a9b1603fe2c08b3d6907aa33bc3223bc7
Mrmoi356 <81328537+Mrmoi356@users.noreply.github.com>
2025-07-13 14:59:03 -0400

    First Commit

--- cml.py
+++ cml.py
@@ -1,46 +1,20 @@
 import requests
 import netmiko
 import mysql.connector
-from mysql.connector import cursor
+from fastapi import HTTPException
 
 from schema import Login, Device
 
-#logs into Cisco Modeling Lab (CML) with username and password
-#function cml_login. Accepts 'info' of type Login
+
+# logs into Cisco Modeling Lab (CML) with username and password
+# function cml_login. Accepts 'info' of type Login
 def cml_login(info: Login):
-    #Login info parameters defined in schema
-    CML_URL = info.cml_url
-    user = info.username
-    password = info.pwd
-
-    #constructs full API endpoint for login: expects a POST request
-    url = CML_URL + "/api/v0/authenticate"
-
-    #request library to send POST request
-    #send credentials as JSON payload
-    #no verification yet (verify = False)
-    response = requests.post(
-        url,
-        json={
-            "username": user,
-            "password": password
-        },
-        verify=False
-    )
-    #server responds with 200, then success. Token string extracted/used in future requests to other endpoints.
-    #else print error message
-    if response.status_code == 200:
-        token = response.text
-        print("Authenticated with token: " + token)
-    else:
-        print(response.text)
+    return "login done"
 
 
-#takes device input of type Device/defined in schema
-#insert basic info about device (day0) configs
-def day0(device: Device):
-    #connects to MySQL DB server running on localhost
-    # (127.0.0.1), port 3306, etc
+# takes device input of type Device/defined in schema
+# insert basic info about device (day0) configs
+def get_deployed():
     conn = mysql.connector.connect(
         host="127.0.0.1",
         port=3306,
@@ -49,14 +23,28 @@ def day0(device: Device):
         database="AutoNetOps"
     )
 
-    #cursor object: allows execution of SQL commands
-    #insert values into device
+    db_cursor = conn.cursor()
+    db_cursor.execute("SELECT * FROM device")
+    rows = db_cursor.fetchall()
+    db_cursor.close()
+    conn.close()
+    return rows
+
+
+def deploy(device: Device):
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="password",
+        database="AutoNetOps"
+    )
+
     db_cursor = conn.cursor()
     sql = """
     INSERT INTO device (hostname, ip_address, platform, username, password)
     VALUES (%s, %s, %s, %s, %s)
     """
-    # tuple of values
     values = (device.hostname,
               device.ip_address,
               device.platform,
@@ -70,4 +58,43 @@ def day0(device: Device):
     db_cursor.close()
     conn.close()
 
-    
+
+def edit_onboard(device_id: int, device: Device):
+    conn = mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="password",
+        database="AutoNetOps"
+    )
+
+    db_cursor = conn.cursor()
+    db_cursor.execute("SELECT * FROM device WHERE device_id = %s", (device_id,))
+
+    if db_cursor.fetchone() is None:
+        conn.close()
+        raise HTTPException(status_code=404, detail="Device not found")
+
+    sql = """
+        UPDATE device SET hostname = %s,
+            ip_address = %s,
+            platform = %s,
+            netconf_port = %s,
+            username = %s,
+            password = %s
+        WHERE device_id = %s
+        """
+
+    values = (device.hostname,
+              device.ip_address,
+              device.platform,
+              830,
+              device.username,
+              device.password,
+              device_id)
+
+    db_cursor.execute(sql, values)
+    conn.commit()
+
+    db_cursor.close()
+    conn.close()
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from typing import List
+
+from click import Path
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from cml import get_deployed, deploy, edit_onboard
 from schema import Device
 
 app = FastAPI()
@@ -22,8 +27,27 @@ async def login(username: str = Form(...), password: str = Form(...)):
     return RedirectResponse(url="/dashboard", status_code=303)
 
 
+@app.get("/devices")
+def get_devices():
+    devices = get_deployed()
+    return {"Current Onboarded devices": devices}
+
+
+@app.post("/devices/deploy")
+def deploy_device(devices: List[Device]):
+    for device in devices:
+        deploy(device)
+    return {"Device Deployed"}
+
+
+@app.put("/devices/deploy/{device_id}")
+def edit_device(devices_id: int = Path(...),
+                device: Device = None):
+    edit_onboard(devices_id, device)
+    return {"Deployed Device Edited"}
+
+
 # DASHBOARD PAGE
 @app.get("/dashboard", response_class=HTMLResponse)
 async def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
-
